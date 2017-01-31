34980d78b2797aa542b723bb1a79ffdf2042b671
RobertPaton <r_paton2@fanshaweonline.ca>
2017-01-31 17:24:28 -0500

    Unzipped files
    Files don't need to be zipped

---
+++ js/main.js
@@ -0,0 +1,6 @@
+/*function TalkDogAnimation(){
+var TalkDog_animation_time = new TimelineLite();
+TweenMax.staggerTo(".TalkDog_animation_one", 1, {opacity:0}, 1),
+TweenMax.staggerTo(".TalkDog_animation_one", 1, {opacity:50}, 1),
+TweenMax.staggerTo(".TalkDog_animation_one", 1, {opacity:100}, 1);
+}*/
